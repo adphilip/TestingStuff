688827926697ae6df0434a8f90429262a28cf246
adrian filipescu <adrian.filipescu@gmail.com>
2015-06-07 20:02:13 +0300

    Skeleton project.

---
+++ src/testing/TestingStuff.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package testing;
+
+/**
+ * @author adrian
+ *
+ */
+public class TestingStuff {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
